09a6b397cc49450a04782146bf756560be49088f
alyssiafourali <alyssia.fourali.etu@univ-lille.fr>
2025-05-05 14:57:30 +0200

    Add functions import

--- src/video_processing_toolkit/__init__.py
+++ src/video_processing_toolkit/__init__.py
@@ -3,3 +3,5 @@
 __author__ = """Alyssia Fourali, Loic Scoth, Mohamed Said Aimen Lamri, Gaspar Henniaux"""
 __email__ = 'alyssia.fourali.etu@univ-lille.fr'
 __version__ = '0.1.0'
+
+from .core import *
