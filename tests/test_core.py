01566ff952ddb474d287f027861a2f9e163679d9
alyssiafourali <alyssia.fourali.etu@univ-lille.fr>
2025-05-06 12:47:15 +0200

    test: Add tests for save_all_p_keyframes

--- tests/test_core.py
+++ tests/test_core.py
@@ -61,3 +61,57 @@ def test_save_all_i_keyframes_creates_jpg_files(tmp_path):
     output_files = list(output_dir.iterdir())
 
     assert all(f.suffix.lower() == ".jpg" for f in output_files)
+
+def test_save_all_i_keyframes_gets_only_iframes(tmp_path):
+    video_path = "tests/video_test.mp4"
+    output_dir = tmp_path
+
+    save_all_i_keyframes(video_path, str(output_dir))
+
+    output_files = list(output_dir.iterdir())
+
+    frame_numbers = [
+        int(f.name.split("frame_")[1].split(".")[0])
+        for f in output_files
+    ]
+
+    frame_types = dict(get_frame_types(video_path))
+
+    assert all(frame_types[num] == 'I' for num in frame_numbers)    
+
+def test_save_all_p_keyframes_not_empty(tmp_path):
+    video_path = "tests/video_test.mp4"
+    output_dir = tmp_path
+
+    save_all_p_keyframes(video_path, str(output_dir))
+
+    output_files = list(output_dir.iterdir())
+
+    assert len(output_files) > 0
+
+def test_save_all_p_keyframes_creates_jpg_files(tmp_path):
+    video_path = "tests/video_test.mp4"
+    output_dir = tmp_path
+
+    save_all_p_keyframes(video_path, str(output_dir))
+
+    output_files = list(output_dir.iterdir())
+
+    assert all(f.suffix.lower() == ".jpg" for f in output_files)
+    
+def test_save_all_p_keyframes_gets_only_pframes(tmp_path):
+    video_path = "tests/video_test.mp4"
+    output_dir = tmp_path
+
+    save_all_p_keyframes(video_path, str(output_dir))
+
+    output_files = list(output_dir.iterdir())
+
+    frame_numbers = [
+        int(f.name.split("frame_")[1].split(".")[0])
+        for f in output_files
+    ]
+
+    frame_types = dict(get_frame_types(video_path))
+
+    assert all(frame_types[num] == 'P' for num in frame_numbers)    
